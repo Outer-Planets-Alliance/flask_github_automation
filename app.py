b102acadba08fc0960b8b16556bad63313912e4d
Brett <brkuhlma@microsoft.com>
2022-01-26 09:42:45 -0500

    reverting

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def webhook():
                 print(response_1.status_code)
     return "OK"
 
-#if __name__ == "__main__":
-#    application.run(host='0.0.0.0')
+if __name__ == '__main__':
+    application.run(debug=True, host='0.0.0.0')
